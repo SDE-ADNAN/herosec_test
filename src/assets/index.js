fd6a1cf0dd603096670345abc5533c11700b38ca
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-03-16 22:41:14 +0530

    static svgs

---
+++ src/assets/index.js
@@ -0,0 +1,20 @@
+import emoji1 from "./1F600.svg"
+import emoji2 from "./1F601.svg"
+import emoji3 from "./1F602.svg"
+import emoji4 from "./1F603.svg"
+import emoji5 from "./1F60E.svg"
+import emoji6 from "./1F605.svg"
+import emoji7 from "./1F606.svg"
+import emoji8 from "./1F607.svg"
+import emoji9 from "./1F609.svg"
+import emoji10 from "./1F60A.svg"
+import emoji11 from "./1F618.svg"
+import emoji12 from "./1F61C.svg"
+import emoji13 from "./1F643.svg"
+import emoji14 from "./1F911.svg"
+import emoji15 from "./1F923.svg"
+import emoji16 from "./1F929.svg"
+
+const emojis = [emoji1,emoji2,emoji3,emoji4,emoji5,emoji6,emoji7,emoji8,emoji9,emoji10,emoji11,emoji12,emoji13,emoji14,emoji15,emoji16]
+    
+export default  emojis
