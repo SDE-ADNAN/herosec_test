02916649fc058c3bc2df32735c30d9524f4587a0
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-03-16 20:27:41 +0530

    added needed jsx

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import "./styles.scss";
+import profile from "./viewPhoto.jpeg";
+
+export default function App() {
+  return (
+    <div className="main">
+      <div className="image box">
+        <img className="" src={profile} alt="profile" />
+      </div>
+      <ul class="circle-container">
+        <li>
+          <div>
+            <span role="img">🐙</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span role="img">🐙</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span role="img">🐙</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span role="img">🐙</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span role="img">🐙</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span role="img">🐙</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span role="img">🐙</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span role="img">🐙</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span role="img">🐙</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span role="img">🐙</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span role="img">🐙</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span role="img">🐙</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span role="img">🐙</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span role="img">🐙</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
