80b2076c1aebf0adaa7939aa12d8c063b7986bd5
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-03-16 22:40:52 +0530

    dynamic emojis

--- src/App.js
+++ src/App.js
@@ -1,83 +1,23 @@
 import "./styles.scss";
 import profile from "./viewPhoto.jpeg";
+import emojis from "./assets"
 
 export default function App() {
+
+
   return (
     <div className="main">
       <div className="image box">
         <img className="" src={profile} alt="profile" />
       </div>
-      <ul class="circle-container">
-        <li>
-          <div>
-            <span role="img">🐙</span>
-          </div>
-        </li>
-        <li>
-          <div>
-            <span role="img">🐙</span>
-          </div>
-        </li>
-        <li>
-          <div>
-            <span role="img">🐙</span>
-          </div>
-        </li>
-        <li>
-          <div>
-            <span role="img">🐙</span>
-          </div>
-        </li>
-        <li>
-          <div>
-            <span role="img">🐙</span>
-          </div>
-        </li>
-        <li>
-          <div>
-            <span role="img">🐙</span>
-          </div>
-        </li>
-        <li>
-          <div>
-            <span role="img">🐙</span>
-          </div>
-        </li>
-        <li>
-          <div>
-            <span role="img">🐙</span>
-          </div>
-        </li>
-        <li>
-          <div>
-            <span role="img">🐙</span>
-          </div>
-        </li>
-        <li>
-          <div>
-            <span role="img">🐙</span>
-          </div>
-        </li>
-        <li>
-          <div>
-            <span role="img">🐙</span>
-          </div>
-        </li>
-        <li>
-          <div>
-            <span role="img">🐙</span>
-          </div>
-        </li>
-        <li>
-          <div>
-            <span role="img">🐙</span>
-          </div>
-        </li>
-        <li>
-          <div>
-            <span role="img">🐙</span>
-          </div>
-        </li>
+      <ul className="circle-container">
+        {emojis.map((emo,index)=>
+        <li key={index}>
+        <div>
+          <img src={emo}/>
+        </div>
+      </li>
+        )}
       </ul>
     </div>
   );
