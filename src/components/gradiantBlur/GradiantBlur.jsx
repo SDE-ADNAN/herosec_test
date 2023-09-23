acf63a8c91b8b7286e74730658e323138516dc92
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-09-23 23:15:48 +0530

    removed comment

--- src/components/gradiantBlur/GradiantBlur.jsx
+++ src/components/gradiantBlur/GradiantBlur.jsx
@@ -12,58 +12,7 @@ const GradiantBlur = () => {
                 {items.map((item, index) => (
                     <li key={index}>Item {index}</li>
                 ))}
-                {/* <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-                <li>Item 4</li>
-                <li>Item 5</li>
-                <li>Item 6</li>
-                <li>Item 7</li>
-                <li>Item 8</li>
-                <li>Item 9</li>
-                <li>Item 10</li>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-                <li>Item 4</li>
-                <li>Item 5</li>
-                <li>Item 6</li>
-                <li>Item 7</li>
-                <li>Item 8</li>
-                <li>Item 9</li>
-                <li>Item 10</li>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-                <li>Item 4</li>
-                <li>Item 5</li>
-                <li>Item 6</li>
-                <li>Item 7</li>
-                <li>Item 8</li>
-                <li>Item 9</li>
-                <li>Item 10</li>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-                <li>Item 4</li>
-                <li>Item 5</li>
-                <li>Item 6</li>
-                <li>Item 7</li>
-                <li>Item 8</li>
-                <li>Item 9</li>
-                <li>Item 10</li>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-                <li>Item 4</li>
-                <li>Item 5</li>
-                <li>Item 6</li>
-                <li>Item 7</li>
-                <li>Item 8</li>
-                <li>Item 9</li>
-                <li>Item 10</li> */}
             </ul>
-            
         </div>
         <div class="gradient-blur">
         <div></div>
@@ -73,7 +22,6 @@ const GradiantBlur = () => {
         <div></div>
         <div></div>
     </div></>
-        
     )
 }
 
